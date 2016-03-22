f6a041a8d237244b6a43f7b968e94aa0c9b02f1c
Alexander von Below <Alex@vonBelow.Com>
2016-03-22 14:05:52 +0100

    Initial Commit

---
+++ ObjectiveSwiftCLI/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ObjectiveSwiftCLI
+//
+//  Created by Alexander v. Below on 22.03.16.
+//  Copyright © 2016 Alexander von Below. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
